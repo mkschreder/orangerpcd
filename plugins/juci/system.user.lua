fc8e5e90d4171d1da2b76faf8046e01b7a6f1360
Martin Schröder <mkschreder.uk@gmail.com>
2016-02-24 00:07:43 +0100

    system.user: remove bunch of old code and use simpler new implementation

--- plugins/juci/system.user.lua
+++ plugins/juci/system.user.lua
@@ -21,42 +21,22 @@ local function shadow_table()
 	return result; 
 end
 
-local function checkpassword(user, pass)
-	local shadow = juci.readfile("/etc/shadow"); 
-	local shpass = nil; 
-	for line in shadow:gmatch("[^\r\n]+") do 
-		local fields = {}; 
-		for field in line:gmatch("[^:]+") do
-			table.insert(fields, field); 
-		end
-		if(fields[1] == user) then shpass = fields[2]; end
-	end
-	if shpass == nil then return false; end
-	local typ, salt, hash = shpass:match("$([^$]+)$([^$]+)$([^$]+).*"); 
-	local epass = juci.shell("echo %s | openssl passwd -1 -salt "..salt.." -stdin", pass);
-	epass = epass:match("[^\r\n]+"); 
-	if shpass == epass then 
-		return true; 
-	end
-	return false; 
-end
-
 local function user_set_password(opts)
 	if not opts["password"] then return 5; end
-	local session = juci.session.get(opts.sid); 
-	if not session then 
-		return { error = "Could not find session!" }; 
-	end
-	if session.data.username ~= opts.username and not juci.session.access(opts.sid, "passwd", "otheruser", { "write" }) then 
+	local session = SESSION.get();
+	if session.username ~= opts.username and not SESSION.access("juci", "passwd", "otheruser", "w") then 
 		return { error = "Can not change password for another user!" }; 
 	end
-	if not juci.session.access(opts.sid, "passwd", "self", { "write" }) then 
+	if not SESSION.access("juci", "passwd", "self", "w") then 
 		return { error = "Permission denied!" };
 	end
-	if not checkpassword(opts.username, opts.oldpassword) then 
+	local oldchk = juci.shell("awk '/%s/{printf $2}' /etc/juci-shadow", opts.username); 
+	local oldhash = juci.shell("printf %s | sha1sum | awk '{printf $1}'", opts.oldpassword); 
+	if oldhash ~= oldchk then
 		return { error = "Invalid username/password!"}; 
 	end
-	local stdout = juci.shell("echo -e \"%s\" | passwd %s", opts["password"].."\\n"..opts["password"], opts["username"]); 
+	local newhash = juci.shell("printf %s | sha1sum | awk '{printf $1}'", opts.password); 
+	juci.shell("sed -i 's/%s.*/%s %s/g' /etc/juci-shadow", opts.username, opts.username, newhash); 
 	return {
 		["stdout"] = stdout
 	}; 
@@ -64,16 +44,13 @@ end
 
 local function user_list(opts)
 	-- list only users that are configured for rpc logins
-	local all_users = juci.shell("uci show rpcd | grep username | awk -F'=' '{print $2;}'");  
+	local all_users = juci.shell("awk '{print $1;}' /etc/juci-shadow");  
 	local users = {};
-	local listothers = juci.session.access(opts.sid, "passwd", "otheruser", { "write" }); 
-	local session = juci.session.get(opts.sid); 
-	if(not session) then
-		return { users = {}, error = "Session not found! ("..(opts.sid or "")..")"};
-	end
+	local listothers = SESSION.access("juci", "passwd", "otheruser", "w"); 
+	local session = SESSION.get(); 
 	for user in all_users:gmatch("[^\r\n]+") do
 		if(not listothers) then 
-			if(session.data.username == user) then table.insert(users, user); end
+			if(session.username == user) then table.insert(users, user); end
 		else
 			table.insert(users, user); 
 		end
