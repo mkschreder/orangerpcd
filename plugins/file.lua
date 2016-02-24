fbc41d239a4a8fed5aec81fbeff7776e39d84a29
Martin Schröder <mkschreder.uk@gmail.com>
2016-02-24 22:19:38 +0100

    file: accept filename (will be checked for access permission by the server)

--- plugins/file.lua
+++ plugins/file.lua
@@ -1,7 +1,7 @@
 #!/usr/bin/lua
 
 local function file_write(opts)
-	fs.writeFragment("/tmp/firmware.bin", opts.seek, opts.length, opts.data64); 
+	fs.writeFragment(opts.filename, opts.seek, opts.length, opts.data64); 
 	return {}; 
 end
 
