ff56d716f513b42d49943c6c2b3769d8782429b7
Martin Schröder <mkschreder.uk@gmail.com>
2016-08-19 13:17:17 +0200

    add hooks support to password script

--- orangectl.sh
+++ orangectl.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 SHADOW=/etc/orange/shadow
+HOOKS=/usr/lib/orange/hooks/
 
 if [ $(whoami) != "root" ]; then
 	echo "Need to be root!"; 
 	exit
 fi
+
+if [ ! -d ${HOOKS} ]; then mkdir -p ${HOOKS}; fi
+chmod 600 ${HOOKS}
 
 while (( "$#" )); do
 	if [ "$1" == "passwd" ]; then
@@ -13,6 +17,7 @@ while (( "$#" )); do
 			SHA1SUM=$(printf "$3" | sha1sum | cut -f 1 -d' ')
 			sed -i "s/^$2.*$/$2 ${SHA1SUM}/g" ${SHADOW}
 			echo "Password for user $2 changed!"; 
+			for file in $(find ${HOOKS}/ -name "*on_passwd_changed*"); do . ${file} $2 $3; done
 		else 
 			echo "User does not exist!"
 		fi
