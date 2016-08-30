4b76a60ed98e8f2bb35c9a2f6e7127cca7bbb773
Martin Schröder <mkschreder.uk@gmail.com>
2016-08-30 23:50:43 +0200

    update orangectl script to work with ash

--- orangectl.sh
+++ orangectl.sh
@@ -1,27 +1,41 @@
-#!/bin/bash
+#!/bin/sh
 
 SHADOW=/etc/orange/shadow
 HOOKS=/usr/lib/orange/hooks/
 
-if [ $(whoami) != "root" ]; then
+if [ $(which whoami) != "" ] && [ $(whoami) != "root" ]; then
 	echo "Need to be root!"; 
 	exit
 fi
 
 if [ ! -d ${HOOKS} ]; then mkdir -p ${HOOKS}; fi
+if [ ! -f ${SHADOW} ]; then touch ${SHADOW}; fi
+
+chown root:root ${SHADOW}
 chown root:root ${HOOKS}
 chmod 600 ${HOOKS}
 
-while (( "$#" )); do
-	if [ "$1" == "passwd" ]; then
-		if [ "" != $(grep "^$2" ${SHADOW} | cut -f 1 -d' ') ]; then 
+while test "$#"; do
+	if [ "$1" = "passwd" ]; then
+		if [ "" != "$(grep "^$2" ${SHADOW} | cut -f 1 -d' ')" ]; then 
 			SHA1SUM=$(printf "$3" | sha1sum | cut -f 1 -d' ')
 			sed -i "s/^$2.*$/$2 ${SHA1SUM}/g" ${SHADOW}
 			echo "Password for user $2 changed!"; 
 			for file in $(find ${HOOKS}/ -name "*on_passwd_changed*"); do . ${file} $2 $3; done
+			exit 0
 		else 
 			echo "User does not exist!"
 		fi
+	elif [ "$1" = "adduser" ]; then
+		if [ "$2" = "" ]; then 
+			echo "No username specified!"
+			exit 1
+		fi
+		if [ "" != "$(grep "^$2" ${SHADOW} | cut -f 1 -d' ')" ]; then
+			echo "User already exists!"; 
+			exit 1; 
+		fi
+		echo "$2 -" >> ${SHADOW}
 	fi
-	shift 
+	if [ "$#" -gt 0 ]; then shift; else break; fi
 done
