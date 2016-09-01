e4ccbee7a18ab7b70ab7941b1f574b137801612b
Martin Schröder <mkschreder.uk@gmail.com>
2016-09-01 02:14:02 +0200

    add -lrt for building on travis and extended session unit test

--- test/session.c
+++ test/session.c
@@ -17,5 +17,15 @@ int main(){
 	TEST(orange_session_access(ses, "myscope", "myobject", "mymethod", "r")); 
 	TEST(orange_session_access(ses, "myscope", "myobject", "mymethod", "w")); 
 	
+	TEST(orange_session_grant(ses, "uci", "net*", "*", "rw") == 0); 
+	TEST(orange_session_access(ses, "uci", "network", "*", "w")); 
+	TEST(!orange_session_access(ses, "uci", "newwork", "*", "w")); 
+	TEST(orange_session_access(ses, "uci", "network", "*", "rw")); 
+
+	TEST(!orange_session_access(ses, "uci", "network", "*", "rwx")); 
+	TEST(!orange_session_access(ses, "uci", "network", "*", "x")); 
+	TEST(orange_session_grant(ses, "uci", "net*", "*", "x") == 0); 
+	TEST(orange_session_access(ses, "uci", "network", "*", "x")); 
+
 	return 0; 
 }
