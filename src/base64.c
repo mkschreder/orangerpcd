5dc8d21cf69a4f50f0757b93a9961c368b4f3d94
Martin Schröder <mkschreder.uk@gmail.com>
2016-09-07 17:39:33 +0200

    base64: change a few variables from char to int and also add -Wtype-limits to makefile

--- src/base64.c
+++ src/base64.c
@@ -128,12 +128,12 @@ static int base64_encode_blockend(char* code_out, base64_encodestate* state_in){
 	return codechar - code_out;
 }
 
-static int base64_decode_value(char value_in){
+static int base64_decode_value(int value_in){
 	static const char decoding[] = {62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-2,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51};
 	static const char decoding_size = sizeof(decoding);
 	value_in -= 43;
 	if (value_in < 0 || value_in > decoding_size) return -1;
-	return decoding[(int)value_in];
+	return decoding[value_in];
 }
 
 static void base64_init_decodestate(base64_decodestate* state_in){
@@ -144,7 +144,7 @@ static void base64_init_decodestate(base64_decodestate* state_in){
 static int base64_decode_block(const char* code_in, const int length_in, char* plaintext_out, size_t size_out, base64_decodestate* state_in){
 	const char* codechar = code_in;
 	char* plainchar = plaintext_out;
-	char fragment;
+	int fragment;
 	
 	*plainchar = state_in->plainchar;
 	
