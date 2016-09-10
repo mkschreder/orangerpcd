d0015d021b926803b42507faaefc18468f43d372
Martin Schröder <mkschreder.uk@gmail.com>
2016-09-10 16:17:06 +0200

    add util.h

---
+++ src/util.h
@@ -0,0 +1,6 @@
+#pragma once
+
+char* shell_command(const char *cmd, int *exit_code); 
+void timespec_now(struct timespec *t); 
+int timespec_before(struct timespec *a, struct timespec *before_b); 
+void timespec_from_now_us(struct timespec *t, unsigned long long timeout_us); 
