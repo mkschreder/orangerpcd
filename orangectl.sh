5a0fca21509935564b107236b98c21b4658b8da7
Martin Schröder <mkschreder.uk@gmail.com>
2016-09-26 12:42:53 +0200

    add serveral new commands to the orangectl script
    - rmuser: remove a user or all users (if user is *)
    - addacl: add an acl to a user
    - rmacl: remove an acl from a user or all acls

--- orangectl.sh
+++ orangectl.sh
@@ -3,20 +3,47 @@
 SHADOW=/etc/orange/shadow
 HOOKS=/usr/lib/orange/hooks/
 
+CONFIG=orange
+
 if [ "$(which whoami)" != "" ] && [ $(whoami) != "root" ]; then
 	echo "Need to be root!"; 
 	exit
 fi
 
-if [ ! -d ${HOOKS} ]; then mkdir -p ${HOOKS}; fi
-if [ ! -f ${SHADOW} ]; then touch ${SHADOW}; fi
+fixup_env() {
+	if [ ! -d ${HOOKS} ]; then mkdir -p ${HOOKS}; fi
+	if [ ! -f ${SHADOW} ]; then touch ${SHADOW}; fi
 
-chown root:root ${SHADOW}
-chown root:root ${HOOKS}
-chmod 600 ${HOOKS}
+	chown root:root ${SHADOW}
+	chown root:root ${HOOKS}
+	chmod 600 ${HOOKS}
+}
 
-while test "$#"; do
-	if [ "$1" = "passwd" ]; then
+ensure_consistency(){
+	if [ ! "$(which uci)" ]; then return; fi
+	# ensure that all the users that are in shadow file are also in the uci file
+	for user in $(cut -f 1 -d' ' ${SHADOW}); do
+		if [ "$(uci get ${CONFIG}.$user)" ]; then continue; fi
+		uci add ${CONFIG} login
+		uci rename ${CONFIG}.@login[-1]=$user
+		uci commit
+	done
+}
+
+# check if user exists
+# 1: username
+check_user(){
+	if [ "$(grep "^$1" ${SHADOW} | cut -f 1 -d' ')" ]; then 
+		echo "$1"
+	fi 
+}
+
+fixup_env
+ensure_consistency
+
+case $1 in 
+	passwd)
+		# ensure that there is a line in the shadow file for this user
 		if [ "" != "$(grep "^$2" ${SHADOW} | cut -f 1 -d' ')" ]; then 
 			if [ "$(which sha1sum)" = "" ]; then 
 				echo "No sha1sum utility present on this system!"; 
@@ -26,20 +53,76 @@ while test "$#"; do
 			sed -i "s/^$2.*$/$2 ${SHA1SUM}/g" ${SHADOW}
 			echo "Password for user $2 changed!"; 
 			for file in $(find ${HOOKS}/ -name "*on_passwd_changed*"); do . ${file} $2 $3; done
-			exit 0
 		else 
 			echo "User does not exist!"
 		fi
-	elif [ "$1" = "adduser" ]; then
-		if [ "$2" = "" ]; then 
+	;;
+	adduser)
+		if [ ! "$2" ]; then 
 			echo "No username specified!"
 			exit 1
 		fi
-		if [ "" != "$(grep "^$2" ${SHADOW} | cut -f 1 -d' ')" ]; then
+		if [ $(check_user $2) ]; then
 			echo "User already exists!"; 
 			exit 1; 
 		fi
+		if [ $(which uci) ]; then
+			uci -q add ${CONFIG} login > /dev/null
+			uci rename ${CONFIG}.@login[-1]=$2
+			uci commit
+		fi
 		echo "$2 -" >> ${SHADOW}
-	fi
-	if [ "$#" -gt 0 ]; then shift; else break; fi
-done
+	;;
+	rmuser)
+		if [ ! "$(which uci)" ]; then exit 0; fi
+		if [ "$2" = "*" ]; then
+			while test "$(uci -q get orange.@login[0])"; do
+				uci delete orange.@login[0]
+				uci commit
+			done
+			echo "" > ${SHADOW}
+		else
+			uci delete orange.$2; 
+			uci commit
+			sed -i '/^$2[:space:].*/d' ${SHADOW}
+		fi
+	;;
+	addacl)
+		if [ ! "$2" ] || [ ! "$3" ]; then
+			echo "Usage: addacl <user> <acl>"; 
+			exit 1; 
+		fi
+		if [ ! $(check_user $2) ]; then
+			echo "User does not exist!"
+			exit 1; 
+		fi
+		if [ ! $(which uci) ]; then
+			exit 0; 
+		fi
+		uci add_list ${CONFIG}.$2.acls=$3
+		uci commit
+	;;
+	rmacl)
+		if [ ! "$2" ] || [ ! "$3" ]; then
+			echo "Usage: rmacl <user> [<acl>|*]"; 
+			exit 1; 
+		fi
+		if [ ! $(check_user $2) ]; then
+			echo "User does not exist!"
+			exit 1; 
+		fi
+		if [ ! $(which uci) ]; then
+			exit 0; # exit with success. Nothing to do. 
+		fi
+		if [ "$3" = "*" ]; then
+			uci delete ${CONFIG}.$2
+		else
+			uci del_list ${CONFIG}.$2.acls=$3
+		fi
+		uci commit
+	;;
+esac
+
+#while test "$#"; do
+#	if [ "$#" -gt 0 ]; then shift; else break; fi
+#done
